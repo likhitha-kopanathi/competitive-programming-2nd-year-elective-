bd42ae4d9823e9ffe168c3d358eb5a80e9886f97
likhitha-kopanathi <kopanathilikhitha1@msitprogram.net>
2021-07-13 08:59:17 +0530

    bonusplay

--- 11-bonusplaythreediceyahtzee-Python/bonusplaythreediceyahtzee.py
+++ 11-bonusplaythreediceyahtzee-Python/bonusplaythreediceyahtzee.py
@@ -19,12 +19,10 @@
 # top-down design and break it up into smaller, more manageable pieces. So we'll first write some 
 # helper functions that do part of the work, and then those helper functions will make our final 
 # function much easier to write. 
-
 # Also note: we will represent a hand of 3 dice as a single 3-digit integer. So the hand 4-3-2 will 
 # be represented by the integer 432. With that, let's start writing some code. Be sure to write 
 # your functions in the same order as given here, since later functions will make use of earlier 
 # ones!
-
 # we've made it to the last function: bonusPlayThreeDiceYahtzee(dice), the function that actually earns 
 # the 2.5 bonus points! This function takes one value, the dice with all the rolls for a game of 3-Dice 
 # Yahtzee. The function plays the game -- it does step1 and gets the first 3 dice (from the right), then it 
@@ -38,7 +36,74 @@
 # assert(bonusPlayThreeDiceYahtzee(2333413) == (333, 29))
 # assert(bonusPlayThreeDiceYahtzee(2333555) == (555, 35))
 
+def handtodice(hand):
+	# your code goes here
+	digit1 = hand//100
+	digit2 = (hand%100)//10
+	digit3 = hand%10
+	return digit1, digit2, digit3
+def dicetoorderedhand(a, b, c):
+	# your code goes here
+	largest = max(a,b,c)
+	smallest = min(a,b,c)
+	medium = a+b+c-largest-smallest
+	return largest*(10**2) + medium*10 +smallest
+def casenum(hand):
+	digit1, digit2, digit3 = handtodice(hand)
+	if digit1==digit2==digit3:
+		return 3
+	elif digit1!=digit2!=digit3:
+		return 1
+	else:
+		return 2
+def playstep2(hand, dice):
+	# your code goes here
+	case = casenum(hand)
+	a,b,c = handtodice(hand)
+	orderedHand = dicetoorderedhand(a,b,c)
+	#case1
+	if case == 3:
+		return hand,dice
+	#case2
+	elif case == 2:
+		num1,num2,num3 = handtodice(orderedHand)
+		num4 = dice%10
+		if num1 == num2:
+			newHand = dicetoorderedhand(num1, num2, num4)
+			return newHand, dice//10
+		elif num1 == num3:
+			newHand = dicetoorderedhand(num1,num3,num4)
+			return newHand, dice//10
+		else:
+			newHand = dicetoorderedhand(num3, num2, num4)
+			return newHand, dice//10
+	#case3
+	else:
+		num1, num2, num3 = handtodice(orderedHand)
+		nextrolls = dice%100
+		n1, n2, n3 = handtodice(num1*100 + nextrolls)
+		newHand = dicetoorderedhand(n1, n2, n3)
+		return newHand, dice//100
+def score(hand):
+	rank = casenum(hand)
+	num1, num2, num3 = handtodice(hand)
+	if rank == 3:
+		return 20+3*(hand//100)
+	elif rank == 2:
+		if num1==num2:
+			return 10+2*num1
+		elif num1==num3:
+			return 10+2*num3
+		else:
+			return 10+2*num2
+	else: return max(num1, num2, num3)
+
 
 def bonusplaythreediceyahtzee(dice):
 	# Your code goes here
-	pass
+	d = dice//1000
+	hand = dice%1000
+	hand1, d1 = playstep2(hand, d)
+	hand2, d2 = playstep2(hand1, d1)
+	scr = score(hand2)
+	return hand2, scr
