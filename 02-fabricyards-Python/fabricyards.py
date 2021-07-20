27aef16b0d611873f52db1e0339b3c0c0b2d3312
likhitha-kopanathi <kopanathilikhitha1@msitprogram.net>
2021-07-20 16:01:56 +0530

    fabricyards and fabricexcess

--- 02-fabricyards-Python/fabricyards.py
+++ 02-fabricyards-Python/fabricyards.py
@@ -14,8 +14,13 @@
 
 def fabricyards(inches):
 	# Your code goes here...
-	return 1
+	if(inches%36 == 0):
+		return inches//36
+	else:
+		return (inches//36 +1)
 
 def fabricexcess(inches):
 	# Your code goes here...
-	return 1
+	if(inches%36 == 0):
+		return 0
+	return 36-(inches%36)
