0b1f150455e12e500654fbcffea721d73791062e
likhitha-kopanathi <kopanathilikhitha1@msitprogram.net>
2021-07-21 11:09:37 +0530

    alternatingsum

--- 05-alternatingsum-Python/alternatingsum.py
+++ 05-alternatingsum-Python/alternatingsum.py
@@ -2,10 +2,7 @@
 # alternating sum (where the sign alternates from positive to negative or vice versa). 
 # For example, alternatingSum([5,3,8,4]) returns 6 (that is, 5-3+8-4). If the list is empty, return 0.
 
-
-
-
 def fun_alternatingsum(a): 
-	return 0
+	return sum(a[::2])-sum(a[1::2])
 
 
