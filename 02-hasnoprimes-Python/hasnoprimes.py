867b65f3a6c6133fd42c213d3f80d3d242e9c247
likhitha-kopanathi <kopanathilikhitha1@msitprogram.net>
2021-07-30 18:34:24 +0530

    hasnoprimes

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -2,6 +2,21 @@
 # and returns True if L does not contain any primes, and False otherwise.
 
 
+def prime(n):
+	if n<2:
+		return False
+	for i in range(2,n):
+		if(n%i == 0):
+			return False
+	return True
+
 def fun_hasnoprimes(l):
+	for r in range(len(l)):
+		for c in range(len(l[r])):
+			if(prime(l[r][c])):
+				return False
+	return True
+
+
 	return True
 
