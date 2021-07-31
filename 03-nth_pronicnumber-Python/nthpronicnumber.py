045b4bfe98122d1b9434b9696bb2d4e918d04a88
likhitha-kopanathi <kopanathilikhitha1@msitprogram.net>
2021-07-31 18:43:05 +0530

    nthpronicnumber

--- 03-nth_pronicnumber-Python/nthpronicnumber.py
+++ 03-nth_pronicnumber-Python/nthpronicnumber.py
@@ -2,7 +2,20 @@
 # Write the function nthPronicNumber that takes a non-negative int n and returns the nth Pronic 
 # Number. Pronic number is a number which is the product of two consecutive integers, that is, a 
 # number n is a product of x and (x+1).
-
+def pronic(n):
+	y = 0
+	while (y <= n) :
+		if (n == y * (y + 1)):
+			return True
+		y+=1
+	return False
+	
 def nthpronicnumber(n):
 	# Your code goes here
-	pass
+	f = 1
+	g = 0
+	while(f<=n):
+		g+=1
+		if(pronic(g)):
+			f+=1
+	return g
