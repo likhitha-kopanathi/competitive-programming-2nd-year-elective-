479e36154c4c61f97be72743dfeab064290e3f79
likhitha-kopanathi <kopanathilikhitha1@msitprogram.net>
2021-07-31 18:15:18 +0530

    nthsmithnumber

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -6,7 +6,53 @@
 # counted twice, thus making 4 a Smith Number.
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
+def isPrime(num):
+    for i in range(2,num):  
+       if (num % i) == 0:  
+           return False
+       else:  
+           continue
+    return True
+
+def get_prime_factors(n):
+    i = 2
+    sum = 0
+    prime_factors = []
+    while i*i <= n:
+        if n%i == 0:
+            sum+=digitsum(i)
+            prime_factors.append(i)
+            n //= i
+        else:
+            i += 1
+    
+    if n>1:
+        prime_factors.append(n)
+        sum+=digitsum(n)
+    return sum
+
+def digitsum(n):
+    sum = 0
+    while (n > 0):
+        sum += (n % 10)
+        n = int(n/10)
+    return sum        
+          
+ #Function to check whether a number is a Smith Number or not      
+def smith(n):
+    if(isPrime(n)):
+        return False
+    if(digitsum(n) == get_prime_factors(n)):
+        return True
+    return False
+
 
 
 def fun_nth_smithnumber(n):
-    return 1
+    f = 0
+    g = 0
+    while(f<=n):
+        g+=1
+        if(smith(g)):
+            f+=1
+    return g
